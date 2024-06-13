648dbb63dcbe2d1427497799e34c597199f085b3
ruslan-99 <ruslanhuseynov109@gmail.com>
2024-06-13 23:20:46 +0400

    Add files via upload

---
+++ Task1/script.js
@@ -0,0 +1,117 @@
+//1.Multiply 12 with 8, and console the result
+//let a = 12
+//let b = 8
+//let c=a*b
+//console.log(c);
+
+
+//2.Divide 10 by 2 add result to variables x, and console x
+
+
+//let a = 10
+//let b = 2
+//let x=a/b
+//console.log(x);
+
+// 3 Create two variables called num1 and num2, assign the value 20 to num1 and value 17 to num2 then addition(+) them in console.
+ //let num1 = 20
+
+ //let num2 = 17
+
+ //let num3=num1+num2
+ //console.log(num3);
+
+//4 Create variables called name, surname and year assign values to them ('your name', 'your surname', ' your birth of year'),
+  //then add them to varibale name (info) and console info. */
+
+// let name = 'ruslan';
+ //let surName = 'ruslan99';
+//let birthOfYear = '1999';
+//let info = `${name} ${surName}, ${birthOfYear}`;
+//console.log(info);
+
+//let name = 'ruslan';
+ //let surName = 'ruslan99';
+//let birthOfYear = '1999';
+//let info =name+surName+birthOfYear
+//console.log(info);
+
+
+
+//5.Console the remainder(%) when 17 is divided by 12.
+
+//let a=17
+//let b=12
+//let c=a/b
+//console.log(c);
+
+
+//6.Create a varibale called "city name" (camelCase). Assign value Baku, then call variable again
+//  and reassign value Gence,console result.
+//let firstName='Ruslan';
+//let lastName='Huseynov';
+//let age='24';
+//let cityName='baku';
+//let cityName='gence'
+//console.log(cityName);
+
+//Task2
+
+// let a = 1, b = 1;
+// let c = ++a;
+// let d = b++;
+
+
+// console.log(c); // 2
+// console.log(d); // 1
+// console.log(b); // 2
+
+
+//Tsk3
+// let a = 2;
+// let x = 1 + (a *= 2);
+//  console.log(x); 
+//  cavab 5
+
+
+
+ //Task4
+ //console.log('test1', test); // ?
+// {
+//     let test = "something"
+//     console.log('test2', test); // ?
+// }
+// console.log('test3', test);  // ?
+
+
+// console.log('test1', test);  // ?
+// {
+//     var test = "something"
+//     console.log('test2', test); // ?
+// }
+// console.log('test3', test); // ?
+
+
+//task5
+
+//let name = "Ulfat"     string [true]
+
+//  let surname = 'Zakirli' 
+// let year = 2000  
+// year = "2000"  
+// let city;
+// let qualification = null
+// let obj = { name: 'ulfat' }
+// let arr = ['a', 'b', 'c']  
+// console.log(typeof surname);
+// console.log(typeof year);
+// console.log(typeof city);
+// console.log(typeof qualification);
+// console.log(typeof obj);
+// console.log(typeof arr);
+
+
+
+
+ 
+
